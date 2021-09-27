29638ed2f65a8d7c7e3564d8503d11ef9ddac309
saeed <saeednaa@post.bgu.ac.il>
2021-09-28 00:06:44 +0300

    only one 'addon' feature updated!

---
+++ app/src/main/java/com/example/ordertakerfrontend/FrontEnd/MenuSection.java
@@ -0,0 +1,37 @@
+package com.example.ordertakerfrontend.FrontEnd;
+
+public class MenuSection {
+    private String section;
+    private String[] addons;
+    private boolean maxOne;
+
+    public MenuSection(String section, String[] addons, boolean maxOne){
+        this.section = section;
+        this.addons = addons;
+        this.maxOne = maxOne;
+    }
+
+    public void setAddons(String[] addons) {
+        this.addons = addons;
+    }
+
+    public void setSection(String section) {
+        this.section = section;
+    }
+
+    public void setMaxOne(boolean maxOne) {
+        this.maxOne = maxOne;
+    }
+
+    public String getSection() {
+        return section;
+    }
+
+    public String[] getAddons() {
+        return addons;
+    }
+
+    public boolean isMaxOne() {
+        return maxOne;
+    }
+}
